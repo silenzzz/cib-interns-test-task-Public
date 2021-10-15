9bcbebbf81474c0784ebaef3646aa3a49cb3c230
Vadim Guseynov <hgt166782@gmail.com>
2021-10-15 11:47:16 +0300

    Initial commit

---
+++ src/test/java/com/demmage/cibinternstesttask/CibInternsTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demmage.cibinternstesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CibInternsTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
